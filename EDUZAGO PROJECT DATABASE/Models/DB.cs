1cae88476c1cb56430efa3823c4de0c2d136e8a8
Ahmed Gamal <Ahmed.agag.gamal@gmail.com>
2025-12-21 20:33:20 +0200

    done merged success

--- EDUZAGO PROJECT DATABASE/Models/DB.cs
+++ EDUZAGO PROJECT DATABASE/Models/DB.cs
@@ -1,198 +1,11 @@
-﻿namespace EDUZAGO_PROJECT_DATABASE.Models
-namespace EDUZAGO_PROJECT_DATABASE.Models;
+﻿namespace EDUZAGO_PROJECT_DATABASE.Models;
+
 using Microsoft.Data.SqlClient;
-
-
-{
-    using Microsoft.Data.SqlClient;
-    using System.Data;
-
-    public class DB
 using System.Data;
 
 public class DB
-    {
-        private string connectionstring = "Data Source=DESKTOP-SQFUII7;Initial Catalog=EDUZAGO_DB;Integrated Security=True;Trust Server Certificate=True";
-        public SqlConnection con { get; set; }
-
-        public DB()
-        {
-            con = new SqlConnection(connectionstring);
-        }
-        public int Get_StudentCount()
-        {
-            int count = 0;
-            string query = "SELECT COUNT(*) FROM Students";
-            SqlCommand cmd = new SqlCommand(query, con);
-            try
-            {
-                con.Open();
-                count = (int)cmd.ExecuteScalar();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-
-            return count;
-
-        }
-        public int Get_InstructorCount()
-        {
-            int count = 0;
-            string query = "SELECT COUNT(*) FROM Instructors";
-            SqlCommand cmd = new SqlCommand(query, con);
-            try
-            {
-                con.Open();
-                count = (int)cmd.ExecuteScalar();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally 
-            {
-                con.Close();
-            }
-            return count;
-        }
-        public int Get_CourseCount()
-        {
-            int count = 0;
-            string query = "Select Count(*) From Course";
-            SqlCommand cmd = new SqlCommand(query, con);
-            try
-            {
-                con.Open();
-                count = (int)cmd.ExecuteScalar();
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-            return count;
-        }
-        public DataTable GetAllCategories()
-        {
-            DataTable dt = new DataTable();
-            string query = "Select * From Category";
-            SqlCommand cmd = new SqlCommand(query, con);
-            try
-            {
-                con.Open();
-                dt.Load(cmd.ExecuteReader());
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally 
-            {
-                con.Close();
-            }
-            return dt;
-        }
-        public DataTable GetAllCourses()
-        {
-            DataTable dt = new DataTable();
-            string query = "SELECT * FROM Course";
-            SqlCommand cmd = new SqlCommand(query, con);
-            try
-            {
-                con.Open();
-                dt.Load(cmd.ExecuteReader());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-            return dt;
-        }
-        public DataTable SearchCourses(string searchText)
-        {
-            DataTable dt = new DataTable();
-
-            string query = @"SELECT * FROM COURSE
-                     WHERE Title LIKE @search" ;
-
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@search", "%" + searchText + "%");
-            try
-            {
-                con.Open();
-                dt.Load(cmd.ExecuteReader());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-            return dt;
-        }
-        public DataTable GetCoursesByCategory(int Category_ID)
-        {
-            DataTable dt = new DataTable();
-
-            string query = "SELECT * FROM COURSE AND CATEGORY WHERE COURSE.Category_ID = @Cat_ID";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@Cat_ID", Category_ID);
-            try
-            {
-                con.Open();
-                dt.Load(cmd.ExecuteReader());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-
-            return dt;
-        }
-        public void EnrollStudent(int StudentId, string CourseCode)
-        {
-            string query = "INSERT INTO Enrollment(Student_ID,Course_Code,Enrollment_Date) VALUES (@sid, @cc, @edate)";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@sid", StudentId);
-            cmd.Parameters.AddWithValue("@cc", CourseCode);
-            cmd.Parameters.AddWithValue("@edate", DateTime.Now);
-            try
-            {
-                con.Open();
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-        }
-
-        public DataTable GetStudentEnrollments(int StudentId)
-        {
-            DataTable dt = new DataTable();
+{
+    private string connectionstring = "Data Source=DESKTOP-SQFUII7;Initial Catalog=EDUZAGO_DB;Integrated Security=True;Trust Server Certificate=True";
     public SqlConnection con { get; set; }
 
     public DB()
@@ -200,15 +13,215 @@ public class DB
         con = new SqlConnection(connectionstring);
     }
 
+    public int Get_StudentCount()
+    {
+        int count = 0;
+        string query = "SELECT COUNT(*) FROM Students";
+        SqlCommand cmd = new SqlCommand(query, con);
+        try
+        {
+            con.Open();
+            count = (int)cmd.ExecuteScalar();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
 
-        public DataTable GetInstructorCourses(int instructor_id) {
+        return count;
 
+    }
+
+    public int Get_InstructorCount()
+    {
+        int count = 0;
+        string query = "SELECT COUNT(*) FROM Instructors";
+        SqlCommand cmd = new SqlCommand(query, con);
+        try
+        {
+            con.Open();
+            count = (int)cmd.ExecuteScalar();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return count;
+    }
+
+    public int Get_CourseCount()
+    {
+        int count = 0;
+        string query = "Select Count(*) From Course";
+        SqlCommand cmd = new SqlCommand(query, con);
+        try
+        {
+            con.Open();
+            count = (int)cmd.ExecuteScalar();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return count;
+    }
+
+    public DataTable GetAllCategories()
+    {
+        DataTable dt = new DataTable();
+        string query = "Select * From Category";
+        SqlCommand cmd = new SqlCommand(query, con);
+        try
+        {
+            con.Open();
+            dt.Load(cmd.ExecuteReader());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return dt;
+    }
+
+    public DataTable GetAllCourses()
+    {
+        DataTable dt = new DataTable();
+        string query = "SELECT * FROM Course";
+        SqlCommand cmd = new SqlCommand(query, con);
+        try
+        {
+            con.Open();
+            dt.Load(cmd.ExecuteReader());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return dt;
+    }
+
+    public DataTable SearchCourses(string searchText)
+    {
         DataTable dt = new DataTable();
 
+        string query = @"SELECT * FROM COURSE
+                 WHERE Title LIKE @search";
 
+        SqlCommand cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@search", "%" + searchText + "%");
+        try
+        {
+            con.Open();
+            dt.Load(cmd.ExecuteReader());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return dt;
+    }
+
+    public DataTable GetCoursesByCategory(int Category_ID)
+    {
+        DataTable dt = new DataTable();
+
+        string query = "SELECT * FROM COURSE AND CATEGORY WHERE COURSE.Category_ID = @Cat_ID";
+        SqlCommand cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@Cat_ID", Category_ID);
+        try
+        {
+            con.Open();
+            dt.Load(cmd.ExecuteReader());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+
+        return dt;
+    }
+
+    public void EnrollStudent(int StudentId, string CourseCode)
+    {
+        string query = "INSERT INTO Enrollment(Student_ID,Course_Code,Enrollment_Date) VALUES (@sid, @cc, @edate)";
+        SqlCommand cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@sid", StudentId);
+        cmd.Parameters.AddWithValue("@cc", CourseCode);
+        cmd.Parameters.AddWithValue("@edate", DateTime.Now);
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    public DataTable GetStudentEnrollments(int StudentId)
+    {
+        DataTable dt = new DataTable();
+
+        string query = @"SELECT C.*, E.Enrollment_Date
+                 FROM COURSE C
+                 WHERE C.Course_Code = E.Course_Code AND E.Student_ID = @sid";
+        SqlCommand cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@sid", StudentId);
+        try
+        {
+            con.Open();
+            dt.Load(cmd.ExecuteReader());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+
+        return dt;
+    }
+
+    public DataTable GetInstructorCourses(int instructor_id)
+    {
+        DataTable dt = new DataTable();
 
         string q = "select  c.Course_Code,c.Title from course c , iNSTRUCTOR i where c.Instructor_ID = i.@InstructorID";
-;
         SqlCommand cmd = new SqlCommand(q, con);
 
         try
@@ -216,14 +229,10 @@ public class DB
             con.Open();
             cmd.Parameters.AddWithValue("@InstructorID", instructor_id);
             dt.Load(cmd.ExecuteReader());
-
-            
-
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
-            
             throw;
         }
         finally
@@ -231,72 +240,25 @@ public class DB
             con.Close();
         }
         return dt;
-        }
-
+    }
 
     public void Addcourse(Course newcourse)
     {
         string q = "Insert into (Course_Code , Title, Fees, Description, Duration, Category_ID, Admin_ID, Instructor_ID) Values (@ccode,@title,@fee,@des,@duration,@categoryid,@adminid,@instructorid)";
         SqlCommand cmd = new SqlCommand(q, con);
+        cmd.Parameters.AddWithValue("@ccode", newcourse.CourseCode);
+        cmd.Parameters.AddWithValue("@title", newcourse.CourseCode);
+        cmd.Parameters.AddWithValue("@fee", newcourse.CourseCode);
+        cmd.Parameters.AddWithValue("@des", newcourse.CourseCode);
+        cmd.Parameters.AddWithValue("@duration", newcourse.CourseCode);
+        cmd.Parameters.AddWithValue("@categoryid", newcourse.CourseCode);
+        cmd.Parameters.AddWithValue("@adminid", newcourse.CourseCode);
+        cmd.Parameters.AddWithValue("@instructorid", newcourse.CourseCode);
 
-            string query = @"SELECT C.*, E.Enrollment_Date
-                     FROM COURSE C
-                     WHERE C.Course_Code = E.Course_Code AND E.Student_ID = @sid";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@sid", StudentId);
-            try
-            {
-                con.Open();
-                dt.Load(cmd.ExecuteReader());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
         try
         {
             con.Open();
-
-            return dt;
-        }
-        public void AddCategory(Category category)
-        {
-            string query = "Insert Into Category ( CategoryID, CategoryName, Description, Admin_ID) Values (@CategoryID, @CategoryName, @ Description, @Admin_ID)";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@ccode", newcourse.CourseCode);
-            cmd.Parameters.AddWithValue("@title", newcourse.CourseCode);
-            cmd.Parameters.AddWithValue("@fee", newcourse.CourseCode);
-            cmd.Parameters.AddWithValue("@des", newcourse.CourseCode);
-            cmd.Parameters.AddWithValue("@duration", newcourse.CourseCode);
-            cmd.Parameters.AddWithValue("@categoryid", newcourse.CourseCode);
-            cmd.Parameters.AddWithValue("@adminid", newcourse.CourseCode);
-            cmd.Parameters.AddWithValue("@instructorid", newcourse.CourseCode);
-
-            cmd.Parameters.AddWithValue("@CategoryID", category.CategoryID);
-            cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
-            cmd.Parameters.AddWithValue("@Description", category.Description);
-            cmd.Parameters.AddWithValue("@Admin_ID", category.Admin_ID);
-            try
-            {
-                con.Open();
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
             cmd.ExecuteNonQuery();
-
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-
-
         }
         catch (Exception ex)
         {
@@ -307,12 +269,29 @@ public class DB
         {
             con.Close();
         }
-
     }
 
+    public void AddCategory(Category category)
+    {
+        string query = "Insert Into Category ( CategoryID, CategoryName, Description, Admin_ID) Values (@CategoryID, @CategoryName, @ Description, @Admin_ID)";
+        SqlCommand cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+        cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+        cmd.Parameters.AddWithValue("@Description", category.Description);
+        cmd.Parameters.AddWithValue("@Admin_ID", category.Admin_ID);
 
-
-
-
-
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
 }
