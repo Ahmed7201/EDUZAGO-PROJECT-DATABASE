56479a6b789fae5a0b54c3f9495b854260a633fb
abdal <abdal@ABDULLAH-LAPTOP>
2025-12-21 17:16:12 +0200

    fixed typos!

--- EDUZAGO PROJECT DATABASE/Models/DB.cs
+++ EDUZAGO PROJECT DATABASE/Models/DB.cs
@@ -1,17 +1,18 @@
 ﻿namespace EDUZAGO_PROJECT_DATABASE.Models;
 using Microsoft.Data.SqlClient;
+using System.Data;
 {
     public class DB
     {
         private string connectionstring = "Data Source=DESKTOP-SQFUII7;Initial Catalog=EDUZAGO_DB;Integrated Security=True;Trust Server Certificate=True";
-    public SqlConnection con { get; set; };
+    public SqlConnection con { get; set; }
 
 
 
 
         public DB()
         {
-            con= new SqlConnection(connectionsrting);
+            con= new SqlConnection(connectionstring);
         }
     }
 }
