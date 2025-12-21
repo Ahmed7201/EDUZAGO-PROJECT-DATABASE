2ab7b0f9334f9356a15673aab9fb156321444647
Ahmed Gamal <Ahmed.agag.gamal@gmail.com>
2025-12-21 17:14:17 +0200

    Done con

--- EDUZAGO PROJECT DATABASE/Models/DB.cs
+++ EDUZAGO PROJECT DATABASE/Models/DB.cs
@@ -1,6 +1,10 @@
-﻿namespace EDUZAGO_PROJECT_DATABASE.Models
+﻿namespace EDUZAGO_PROJECT_DATABASE.Models;
+using Microsoft.Data.SqlClient;
 {
     public class DB
     {
+        private string connectionstring = "Data Source=DESKTOP-SQFUII7;Initial Catalog=EDUZAGO_DB;Integrated Security=True;Trust Server Certificate=True";
+    public SqlConnection con { get; set; };
+
     }
 }
