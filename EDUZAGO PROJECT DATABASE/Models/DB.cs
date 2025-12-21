4defa22b987032b7ecc529fbdd5c48f8b3ee80f3
abdal <abdal@ABDULLAH-LAPTOP>
2025-12-21 18:12:55 +0200

    4 Functions Done!

--- EDUZAGO PROJECT DATABASE/Models/DB.cs
+++ EDUZAGO PROJECT DATABASE/Models/DB.cs
@@ -12,5 +12,88 @@
         {
             con = new SqlConnection(connectionstring);
         }
+        public int Get_StudentCount()
+        {
+            int count = 0;
+            string query = "SELECT COUNT(*) FROM Students";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                count = (int)cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                // Handle exception (log it, rethrow it, etc.)
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return count;
+
+        }
+        public int Get_InstructorCount()
+        {
+            int count = 0;
+            string query = "SELECT COUNT(*) FROM Instructors";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                count = (int)cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                con.Close();
+            }
+            return count;
+        }
+        public int Get_CourseCount()
+        {
+            int count = 0;
+            string query = "Select Count(*) From Course";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                count = (int)cmd.ExecuteScalar();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return count;
+        }
+        public DataTable GetAllCategories()
+        {
+            DataTable dt = new DataTable();
+            string query = "Select * From Category";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                dt.Load(cmd.ExecuteReader());
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            finally 
+            {
+                con.Close();
+            }
+            return dt;
+        }
     }
 }
