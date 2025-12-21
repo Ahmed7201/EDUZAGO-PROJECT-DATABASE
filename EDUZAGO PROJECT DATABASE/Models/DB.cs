64ece8b4e10fa606cb48c0d001356f1aca8be9a7
abdal <abdal@ABDULLAH-LAPTOP>
2025-12-21 17:19:16 +0200

    fixed error!

--- EDUZAGO PROJECT DATABASE/Models/DB.cs
+++ EDUZAGO PROJECT DATABASE/Models/DB.cs
@@ -1,18 +1,16 @@
-﻿namespace EDUZAGO_PROJECT_DATABASE.Models;
-using Microsoft.Data.SqlClient;
-using System.Data;
+﻿namespace EDUZAGO_PROJECT_DATABASE.Models
 {
+    using Microsoft.Data.SqlClient;
+    using System.Data;
+
     public class DB
     {
         private string connectionstring = "Data Source=DESKTOP-SQFUII7;Initial Catalog=EDUZAGO_DB;Integrated Security=True;Trust Server Certificate=True";
-    public SqlConnection con { get; set; }
-
-
-
+        public SqlConnection con { get; set; }
 
         public DB()
         {
-            con= new SqlConnection(connectionstring);
+            con = new SqlConnection(connectionstring);
         }
     }
 }
